24e375fd57417780c506facbbad949febdd6f040
Mark Cooper <markchristophercooper@gmail.com>
2018-06-26 19:59:05 -0700

    Add LSI to query by updated_at

--- manifest/process.py
+++ manifest/process.py
@@ -8,6 +8,7 @@ import time
 
 
 from pynamodb.models import Model
+from pynamodb.indexes import LocalSecondaryIndex, AllProjection
 from pynamodb.attributes import (
     BooleanAttribute, NumberAttribute, UnicodeAttribute
 )
@@ -65,16 +66,26 @@ class Manifest:
                 yield resource
 
 
+# for r in Resource.updated_at_idx.query(site, Resouce.updated_at > timestamp):
+#     print("Resource queried from index: {0}".format(r.url))
+class ResourceUpdatedAtIndex(LocalSecondaryIndex):
+    class Meta:
+        projection = AllProjection()
+    site = UnicodeAttribute(hash_key=True)
+    updated_at = NumberAttribute(range_key=True)
+
+
 class Resource(Model):
     class Meta:
         table_name = os.getenv('FAD_TABLE_NAME', 'fad')
         region = os.getenv('AWS_REGION')
-    site = UnicodeAttribute()
-    url = UnicodeAttribute(hash_key=True)
+    site = UnicodeAttribute(hash_key=True)
+    url = UnicodeAttribute(range_key=True)
     deleted = BooleanAttribute()
     filename = UnicodeAttribute()
     title = UnicodeAttribute()
-    updated_at = NumberAttribute(range_key=True)
+    updated_at = NumberAttribute()
+    updated_at_idx = ResourceUpdatedAtIndex()
 
 
 def handler(event, context):
@@ -85,7 +96,7 @@ def handler(event, context):
 
     for resource in process(site, location):
         create = False
-        count = Resource.count(resource.url)
+        count = Resource.count(resource.site, Resource.url == resource.url)
 
         if count > 1:
             raise Exception('Duplicate url detected: ' + resource.url)
